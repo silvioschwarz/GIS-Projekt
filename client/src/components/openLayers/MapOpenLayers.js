4374285a79a770760ff74a72b04c52ba85e493b2
Silvio Schwarz <admin@silvioschwarz.com>
2022-06-09 10:50:02 +0200

    changed Map.css
    set overview map size but hide when collapsed -> no more warning that map has width/height 0

--- client/src/components/openLayers/MapOpenLayers.js
+++ client/src/components/openLayers/MapOpenLayers.js
@@ -92,6 +92,25 @@ const MapOpenLayers = () => {
     setDatum(newDatum);
   }
 
+  const doFetchDownload = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(resp => resp.blob())
+      .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        // the filename you want
+        a.download = "todo-1.json";
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        alert("your file has downloaded!"); // or you know, something with better UX...
+      })
+      .catch(() => alert("oh no!"));
+  };
+  
+
   return (
     <div>
       <label htmlFor="test-date">Tag wählen: </label>
